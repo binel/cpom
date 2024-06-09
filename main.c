c9e9c76329019560036f444e8be2c9248469c2d1
Ben Nelson <binelson13@gmail.com>
2024-06-09 18:48:31 -0500

    Kind of working prototype.
    You click to start, and then over the course of 25 minutes the circle
    slowly fills up. I need to make it easier to test this more quickly, and
    also figure out how to compile it for multiple platforms and test on
    windows.

--- main.c
+++ main.c
@@ -1,22 +1,60 @@
 #include <stdio.h>
-
+#include <time.h>
 #include <raylib.h>
 
+void GetTimeString(char* str, double seconds)
+{
+	int secs = (int) seconds;
+	int secondsDisp = secs % 60;
+	int minutesDisp = (secs - secondsDisp) / 60;
+	sprintf(str, "%02d:%02d", minutesDisp, secondsDisp);
+}
+
 int main() {
-	InitWindow(250, 250, "cpom");
+	bool started = false;
+
+	time_t start, current;
+	double elapsed;
+
+	InitWindow(250, 300, "cpom");
 	
 	SetTargetFPS(60);
 	int frames = 0;
+	char timeStr[20];
+	
+	double circlePerSecond = 100.0 / (25.0 * 60.0);
 	
 	while(!WindowShouldClose()) {
-		frames++;
-		frames = frames % 2000;
 		BeginDrawing();
 		
-		ClearBackground(BLACK);
-		DrawCircleLines(125, 125, 100, WHITE);
+		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+			started = true;
+			time(&start);
+		}
 		
-		DrawCircle(125, 125, 0.05 * frames, WHITE);
+		if (!started) {
+			DrawText("Click to start", 15, 125, 32, WHITE);
+		}
+		else {
+			if (elapsed < 25 * 60)
+			{
+				time(&current);		
+			}
+			else {
+				started = false;
+			}
+	
+			elapsed = difftime(current, start);
+		
+			ClearBackground(BLACK);
+			DrawCircleLines(125, 125, 100, WHITE);
+			
+			
+			DrawCircle(125, 125, circlePerSecond * elapsed, WHITE);
+			GetTimeString(timeStr, elapsed);
+			DrawText(timeStr, 90, 250, 32, WHITE);			
+		}
+
 		
 		EndDrawing();
 	}
