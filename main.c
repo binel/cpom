45e54ba80bbccb31ffcb91b17f2927658f13a24d
Ben Nelson <binelson13@gmail.com>
2024-06-10 22:03:17 -0500

    Restart and easier testing
    Making the timer kind of configurable to make testing easier, and
    starting at a restart implementation.
    
    I almost implemented a pause/play button, but I realized that kind of
    goes against the whole ethos of the tool.

--- main.c
+++ main.c
@@ -2,12 +2,27 @@
 #include <time.h>
 #include <raylib.h>
 
-void GetTimeString(char* str, double seconds)
-{
+#define TIMER_SECONDS 1500
+
+void GetTimeString(char* str, double seconds) {
 	int secs = (int) seconds;
 	int secondsDisp = secs % 60;
 	int minutesDisp = (secs - secondsDisp) / 60;
 	sprintf(str, "%02d:%02d", minutesDisp, secondsDisp);
+}
+
+void DrawRestart() {
+	DrawCircle(30, 250, 10, WHITE);
+}
+
+bool IsRestartClicked() {
+	if (!IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+		return false;
+	}
+	
+	Vector2 position = GetMousePosition();
+	
+	return position.x > 15 && position.x < 45 && position.y > 240 && position.y < 270;
 }
 
 int main() {
@@ -22,13 +37,17 @@ int main() {
 	int frames = 0;
 	char timeStr[20];
 	
-	double circlePerSecond = 100.0 / (25.0 * 60.0);
+	double circlePerSecond = 100.0 / ((double)TIMER_SECONDS);
 	
 	while(!WindowShouldClose()) {
 		BeginDrawing();
 		
-		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) & !started) {
 			started = true;
+			time(&start);
+		}
+		
+		if (IsRestartClicked()) {
 			time(&start);
 		}
 		
@@ -36,7 +55,7 @@ int main() {
 			DrawText("Click to start", 15, 125, 32, WHITE);
 		}
 		else {
-			if (elapsed < 25 * 60)
+			if (elapsed < TIMER_SECONDS)
 			{
 				time(&current);		
 			}
@@ -52,7 +71,9 @@ int main() {
 			
 			DrawCircle(125, 125, circlePerSecond * elapsed, WHITE);
 			GetTimeString(timeStr, elapsed);
-			DrawText(timeStr, 90, 250, 32, WHITE);			
+			DrawText(timeStr, 90, 250, 32, WHITE);		
+			
+			DrawRestart();	
 		}
 
 		
